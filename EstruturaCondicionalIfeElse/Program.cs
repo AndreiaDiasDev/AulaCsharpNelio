e65017fb92569bd8e151d1241cddd970d5f63c3a
AndreiaDiasDev <andreiadiasdev@outlook.com>
2024-05-18 17:37:38 -0300

    estudando 18/08 operadores comparativos

---
+++ EstruturaCondicionalIfeElse/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO.Pipes;
+using System.Security.AccessControl;
+
+
+namespace Estrutura
+{
+    class Aprender
+    {
+        static void Main()
+        {
+            /*
+            Console.WriteLine("Vamos decobrir se o seu número é Par ou Ímpar");
+            Console.WriteLine("Digite um número");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 2 == 0)
+            {
+                Console.WriteLine("Seu número é PAR");
+            }
+            else
+            {
+                Console.WriteLine("Seu número e IMPAR");
+            }
+            
+
+            Console.WriteLine("==============================================================================================================");
+            
+            Console.WriteLine("VAMOS SABER SE PELO SEU ANO DE NASCIMENTO VOCE PODE VOTAR?");
+
+            Console.WriteLine("DIGITE SEU ANO DE NASCIMENTO");
+            int anonasc = int.Parse(Console.ReadLine());
+            int idade = 2024 - anonasc;
+
+            if (idade > 18)
+            {
+                Console.WriteLine("Você  pode votar pois tem " + idade);
+
+            }
+
+           else
+            {
+                Console.WriteLine("Você não pode votar pois é menor de idade, voce tem " + idade); Console.WriteLine("anos");
+            }
+            */
+
+            //CLASSIFICAÇÃO DE NOTA
+
+            Console.WriteLine("Qual foi sua nota?");
+            int nota = int.Parse(Console.ReadLine());
+
+            if (nota >=90 )
+            {
+                Console.WriteLine("Excelente");
+            }
+            else if (nota >=80 )
+            {
+                Console.WriteLine("bom");
+            }
+
+            else if (nota >=70)
+            {
+                Console.WriteLine("Regular");
+            }
+
+            else if (nota >=60)
+            {
+                Console.WriteLine("Suficiente");
+            }
+
+        }
+    }
+}
---
+++ OperadoreComparativos/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace Operadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // OPERADORES COMPARATIVOS
+
+            int a = 10;
+            bool c1 = a < 10;
+            bool c2 = a < 20;
+            bool c3 = a > 10;
+            bool c4 = a > 5;
+
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Console.WriteLine(c3);
+            Console.WriteLine(c4);
+
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+
+            bool c5 = a <= 10;
+            bool c6 = a >= 10;
+            bool c7 = a == 10;
+            bool c8 = a != 10;
+
+            Console.WriteLine(c5);
+            Console.WriteLine(c6);
+            Console.WriteLine(c7);
+            Console.WriteLine(c8);
+
+            Console.WriteLine("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+
+            //OPERADORES LOGICOS - && E - || OU - ! NÃO
+
+            bool c9 = 2 > 3 || 4 != 5; //true
+            bool c10 = !(2 > 3) && 4 != 5; //true
+            Console.WriteLine(c9);
+            Console.WriteLine(c10);
+
+
+
+
+
+
+
+        }
+    }
+}
