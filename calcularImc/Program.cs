b18f1e3cfdd5a3032a5fa6887d84c099f6a28890
AndreiaDiasDev <andreiadiasdev@outlook.com>
2024-05-18 14:59:29 -0300

    exercitando variaveis

--- calcularImc/Program.cs
+++ calcularImc/Program.cs
@@ -4,17 +4,74 @@ class Estudar
 {
     static void Main()
     {
-        Console.WriteLine("Vamos calcular o IMC");
+        //CÁLCULO DO IMC
+
+         Console.WriteLine("Vamos calcular o IMC");
+
+         Console.WriteLine("Digite o seu peso");
+         double peso = double.Parse(Console.ReadLine());
+
+         Console.WriteLine("Digite sua altura");
+         double altura = double.Parse(Console.ReadLine());
+
+         double imc = peso / (altura * altura);
+
+         Console.WriteLine("Seu IMC é " +  imc.ToString("F2"));
+
+        Console.WriteLine("=========================================================================================================");
+
         
-        Console.WriteLine("Digite o seu peso");
-        double peso = double.Parse(Console.ReadLine());
+        //CÁLCULO DA MÉDIA
+        Console.WriteLine("VAMOS CALCULAR A MÉDIA");
+        
+        Console.WriteLine("Digite a nota1");
+        int nota1 = int.Parse(Console.ReadLine());
 
-        Console.WriteLine("Digite sua altura");
-        double altura = double.Parse(Console.ReadLine());
+        Console.WriteLine("Digite a nota2");
+        int nota2 = int.Parse(Console.ReadLine());
 
-        double imc = peso / (altura * altura);
+        int media = (nota1 +  nota2) / 2;
 
-        Console.WriteLine("Seu IMC é " +  imc.ToString("F2"));
+        Console.WriteLine("Sua média é " + media);
+
+        Console.WriteLine("==================================================================================================================");
+
+
+        //CONVERTENDO CELSIUS EM FAHRENHEIT
+        Console.WriteLine("CONVERTER TEMPERATURA EM GRAU CELSIUS PARA FAHRENHEIT");
+
+        Console.WriteLine("DIGITE UMA TEMPERATURA");
+        int temperatura = int.Parse(Console.ReadLine());
+
+        int converte = (temperatura * 9/5) + 32;
+
+        Console.WriteLine("FOI CONVERTIDO FAHREHEIT E O NÚMERO É " + converte);
+
+        Console.WriteLine("==================================================================================================================");
+
+
+        Console.WriteLine("CALCULAR A GORJETA");
+
+        Console.WriteLine("Digite o valor total da conta");
+        double vconta = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Digite o percentual de gorjeta para garçom");
+        double percgorjeta = double.Parse(Console.ReadLine());
+
+        double tgorjeta = vconta * (percgorjeta / 100);
+        double tgeral = vconta + tgorjeta;
+        
+
+        Console.WriteLine(" A gorjeta do garçom é R$ "  + tgorjeta);
+        Console.WriteLine(" O valor total é de R$ " + tgeral);
+
+
+
+        
+
+
+
+
 
     }
 }
--- calcularImc/obj/Debug/net8.0/calcularImc.AssemblyInfo.cs
+++ calcularImc/obj/Debug/net8.0/calcularImc.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("calcularImc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d71d3bb304d366afb970622b5440f474b2feed1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e476fd094259cd43cca7f052c9a450dac5a61eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("calcularImc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("calcularImc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
